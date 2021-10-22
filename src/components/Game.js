083208445ecb68692165dce20831a735f89557cc
Kevin Hines <kevhines@gmail.com>
2021-10-22 14:26:31 -0400

    handleclick started

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,4 +8,19 @@ const Game = () => {
     const[xIsNext, setXisNext] = useState(true)
    const winner = calculateWinner(history[stepNUmber])
    const xO = xIsNext ? "X" : "O"
+
+   const handleClick - (i) => {
+        const historyPoint = history.slice(0, stepNumber + 1)
+        const current = historyPoint[stepNUmber]
+        const squares = [...currrent]
+        //return if won or occupied
+        if (winner || squares[i]) return
+        // select squares
+        squares[i] = xO
+        setHistory([...historyPoint, squares])
+        setStepNumber(historyPoint.length)
+        setXisNext(!xIsNext)
+    }
+
+    return (    )
 }
