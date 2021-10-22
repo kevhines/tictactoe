b2705903d034c3a3007cdc0a03cd3f50023904ae
Kevin Hines <kevhines@gmail.com>
2021-10-22 14:30:26 -0400

    return function for game

--- src/components/Game.js
+++ src/components/Game.js
@@ -22,5 +22,20 @@ const Game = () => {
         setXisNext(!xIsNext)
     }
 
-    return (    )
+    return (   
+        <>
+            <h1>React Tic Tac Toe - With Hookes</h1>
+            <Board squares={history[stepNumber]} onClick={handleClick} />
+            <div className="info-wrapper">
+                <div>
+                    <h3>History</h3>
+                    {renderMoves()}
+                </div>
+                <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+            </div>
+
+        </>
+     )
 }
+
+export default Game
